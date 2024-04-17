f5145f135c753e58cbc1adcd80bc86a35cf90b8f
Ignacio Paradisi <ignacioparadisi@gmail.com>
2024-04-17 14:48:50 -0600

    Initial Commit

---
+++ MSG/MSGApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSGApp.swift
+//  MSG
+//
+//  Created by Ignacio Paradisi on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MSGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
